4a848bdb5886d03c1146c1aea465b4a7088d7564
bottobot <105563502+bottobot@users.noreply.github.com>
2024-10-21 16:30:05 -0700

    more fixes for vercel

--- next.config.js
+++ next.config.js
@@ -10,8 +10,17 @@ const nextConfig = {
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
+        net: false,
+        tls: false,
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
+        url: require.resolve('url/'),
+        zlib: require.resolve('browserify-zlib'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        assert: require.resolve('assert/'),
+        os: require.resolve('os-browserify/browser'),
+        path: require.resolve('path-browserify'),
       };
     }
     return config;

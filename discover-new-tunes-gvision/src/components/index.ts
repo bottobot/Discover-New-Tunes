dc4ed35ff6f2dd50446c8b9555c78fba640cf4d6
bottobot <105563502+bottobot@users.noreply.github.com>
2024-10-22 17:39:47 -0700

    redid eveyrthing

--- discover-new-tunes-gvision/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Header } from './Header';
-export { default as MadeBy } from './MadeBy';
-export { default as ContentLeft } from './ContentLeft';
-export { default as ContentRight } from './ContentRight';
-export { default as Lineup } from './Lineup';
-export { default as OCRReview } from './OCRReview';
-export { default as Notification } from './Notification';

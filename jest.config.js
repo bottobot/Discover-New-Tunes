fb0bcf2c8a7eb9a6445d60bcb2a0809ec17b58fc
bottobot <105563502+bottobot@users.noreply.github.com>
2024-10-23 11:19:46 -0700

    fix: update Jest configuration for Next.js compatibility

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '<rootDir>/src/__tests__/**/*.test.ts',
+    '<rootDir>/src/__tests__/**/*.test.tsx'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }]
+  }
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ jest.setup.js
@@ -0,0 +1,2 @@
+// Add any global test setup here
+process.env.NODE_ENV = 'test';
